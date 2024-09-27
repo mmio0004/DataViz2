69284adb0a2bf9dcc66cca197c4b6abfd5cdb3f5
Mior Afiq <mmio0004@student.monash.edu>
2024-09-27 19:13:27 +0800

    modify code

---
+++ js/cvd_lollipop_chart_vis.js
@@ -0,0 +1,7 @@
+var vg_2 = "cvd_death_rate_lollipop_chart.vg.json";
+
+vegaEmbed("#lollipop", vg_2).then(function(result) {
+    
+    //Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
